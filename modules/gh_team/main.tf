a49b5c9f5589817d5a5bc488e1ab875c075c51b4
pathumf <madushanwf@gmail.com>
2024-01-24 11:17:58 +0000

    update comments

--- modules/gh_team/main.tf
+++ modules/gh_team/main.tf
@@ -1,10 +1,12 @@
 resource "github_team" "team" {
+  # Resource to create a GitHub team with specified name, description, and privacy settings
   name        = var.team_name
   description = var.team_description
   privacy     = var.team_privacy
 }
 
 resource "github_team_membership" "members" {
+  # Resource to add members to the GitHub team
   for_each = toset(var.member_list)
   username = each.key
   role     = "member"
@@ -12,6 +14,7 @@ resource "github_team_membership" "members" {
 }
 
 resource "github_team_membership" "maintainers" {
+  # Resource to add maintainers to the GitHub team
   for_each = toset(var.maintainer_list)
   username = each.key
   role     = "maintainer"
